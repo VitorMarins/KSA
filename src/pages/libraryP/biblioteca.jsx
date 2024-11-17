2c302e60133159fd7a3da6385448c75f577e81dc
Carla Dos Santos Romero <Carlasromero11@gmail.com>
2024-11-16 22:02:40 -0300

    commit de organização de pastas

---
+++ src/pages/libraryP/biblioteca.jsx
@@ -0,0 +1,22 @@
+import React, { useState } from 'react';
+import './biblioteca.css';
+import { 
+    HeaderSite,
+    Footer
+  } from '../../imports/imports';
+
+function Biblioteca() {
+    return(
+      <div className='biblioteca'> 
+          <HeaderSite />
+
+          <section className=''>
+              <h1>Biblioteca</h1>
+          </section>
+
+
+      </div>
+    );
+}
+
+export default Biblioteca;
